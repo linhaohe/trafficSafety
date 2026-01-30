7f0f46a457b1b69d2445e9d901574539d43aa90e
linhaohe <linhaohe0914@gmail.com>
2026-01-30 09:30:53 -0500

    revert changes back to previous version

--- main.py
+++ main.py
@@ -23,22 +23,5 @@ def makeDummyTimeData(intend_to_cross_timestamp,crossing_start_time, crossing_en
     return pd.DataFrame(time, index=[0])
 
 if __name__ == "__main__":
-    # Generate graph data for testing different percentage thresholds
-    # generateGraphDataPercentage(start_percent=1, end_percent=101, time_threshold=6)
-    # generateGraphDataTime(start_time=1, end_time=20, percentage_threshold=0.6)
-
-    # Uncomment to generate graphs from existing data
-    # graphData()
-    
-    # Uncomment to run single computation and accuracy tests
     # computeDataFolderToCSV(INPUT_DATA_PATH, OUTPUT_PATH, percentageThreshold=0.65, timeThreshold=6)
     performAccuracyTest(NORTHAMPTON_OUTPUT, NORTHAMPTON_HUMAN_QC)
-    # dummy1 = makeDummyTimeData(55215.0,55216.0,55243.0,-1,-1)
-    # dummy2 = makeDummyTimeData(-1,55290.0,55308.0,-1,-1)
-    # dummy3 = makeDummyTimeData(-1,55222.0,55236.0,-1,-1)
-    # testTimeThreshold = 6
-    # test = computeTimeScore(dummy1.iloc[0], dummy2.iloc[0], testTimeThreshold)
-    # test2 = computeTimeScore(dummy1.iloc[0], dummy3.iloc[0], testTimeThreshold)
-    # print(f"Test: {test}")
-    # print(f"Test2: {test2}")
-    # performAccuracyTest(BELMONT_OUTPUT, BELMONT_HUMAN_QC) 
